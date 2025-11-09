b7936082d37d91bf8f77ba3b75e3272cdc271bb3
St Clair Student <student@stclaircollege.ca>
2025-11-09 08:27:18 -0500

    DAB-1 Add Python script for Jira integration demo

---
+++ app.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    logging.info("Application started successfully.")
+    print("Hello from Jira & GitHub integration demo!")
+
+if __name__ == "__main__":
+    main()
